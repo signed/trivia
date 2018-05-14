830776ca68ab7a7c3b07c9fad450135442368dc1
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2018-05-14 22:31:16 +0200

    open test seam to enable deterministic tests

--- java/src/main/java/com/adaptionsoft/games/trivia/runner/GameRunner.java
+++ java/src/main/java/com/adaptionsoft/games/trivia/runner/GameRunner.java
@@ -10,27 +10,24 @@ public class GameRunner {
 	private static boolean notAWinner;
 
 	public static void main(String[] args) {
+		runGame(new Random());
+	}
+
+	public static void runGame(Random rand) {
 		Game aGame = new Game();
-		
+
 		aGame.add("Chet");
 		aGame.add("Pat");
 		aGame.add("Sue");
-		
-		Random rand = new Random();
-	
+
 		do {
-			
 			aGame.roll(rand.nextInt(5) + 1);
-			
+
 			if (rand.nextInt(9) == 7) {
 				notAWinner = aGame.wrongAnswer();
 			} else {
 				notAWinner = aGame.wasCorrectlyAnswered();
 			}
-			
-			
-			
 		} while (notAWinner);
-		
 	}
 }
