0e16b3935f013f4b10fab4fdece3c82963a329cb
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2018-05-15 23:22:14 +0200

    add golden master setup for groovy

--- groovy/src/main/groovy/com/adaptionsoft/games/trivia/runner/GameRunner.groovy
+++ groovy/src/main/groovy/com/adaptionsoft/games/trivia/runner/GameRunner.groovy
@@ -1,7 +1,6 @@
 
 package com.adaptionsoft.games.trivia.runner
 
-import java.util.Random
 import com.adaptionsoft.games.uglytrivia.Game
 
 public class GameRunner {
@@ -9,28 +8,31 @@ public class GameRunner {
 	private static boolean notAWinner
 
 	public static void main(String[] args) {
-	    	    
+		Random rand = new Random()
+		runGame(rand)
+	}
+
+	public static void runGame(Random rand) {
 		Game aGame = new Game()
-		
+
 		aGame.add("Chet")
 		aGame.add("Pat")
 		aGame.add("Sue")
-		
-		Random rand = new Random()
-	
-	    boolean isFirstRound = true
-	    notAWinner = false
+
+
+		boolean isFirstRound = true
+		notAWinner = false
 		while (isFirstRound || notAWinner) {
-			
+
 			aGame.roll(rand.nextInt(5) + 1)
-			
+
 			if (rand.nextInt(9) == 7) {
 				notAWinner = aGame.wrongAnswer()
 			} else {
 				notAWinner = aGame.wasCorrectlyAnswered()
 			}
-			
+
 			isFirstRound = false
-		} 
+		}
 	}
 }
---
+++ groovy/src/test/groovy/com/adaptionsoft/GoldenMasterTests.groovy
@@ -0,0 +1,68 @@
+package com.adaptionsoft
+
+import com.adaptionsoft.games.trivia.runner.GameRunner
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+
+import java.nio.charset.Charset
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+import java.util.stream.LongStream
+
+import static org.junit.jupiter.api.Assertions.assertEquals
+
+class GoldenMasterTests {
+
+    private final ByteArrayOutputStream byteStream = new ByteArrayOutputStream()
+    private final PrintStream outBackup = System.out
+
+    @BeforeEach
+    void redirectSystemOutToInMemoryBuffer() {
+        PrintStream out = new PrintStream(byteStream)
+        System.setOut(out)
+    }
+
+    @AfterEach
+    void restoreSystemOut() {
+        System.setOut(outBackup)
+    }
+
+    @ParameterizedTest
+    @MethodSource("seeds")
+    void runGoldMasterTests(long seed) throws IOException {
+        assertEquals(recordedOutputFor(seed), gameOutputFor(seed))
+    }
+
+    @ParameterizedTest
+    @MethodSource("seeds")
+    @Disabled("should only be executed manually to record golden master")
+    void recordGoldenMaster(long seed) throws IOException {
+        String s = gameOutputFor(seed)
+        Path outputPath = pathToOutputFileFor(seed)
+        Files.createDirectories(outputPath.getParent())
+        Files.write(outputPath, s.getBytes(Charset.forName("UTF-8")))
+    }
+
+    private static LongStream seeds(){
+        return LongStream.range(0, 255)
+    }
+
+    private String recordedOutputFor(long seed) throws IOException {
+        return new String(Files.readAllBytes(pathToOutputFileFor(seed)), "UTF-8")
+    }
+
+    private Path pathToOutputFileFor(long seed) {
+        return Paths.get("goldenmaster", seed + ".out")
+    }
+
+    private String gameOutputFor(long seed) throws UnsupportedEncodingException {
+        Random random = new Random(seed)
+        GameRunner.runGame(random)
+
+        return byteStream.toString("UTF-8")
+    }
+}
