e9aa9804ec3121df32a858bb25bfbb5f154f504b
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2018-05-21 17:35:10 +0200

    add a junit 5 test

---
+++ kotlin/src/test/kotlin/com/adaptionsoft/games/trivia/SomeJupiterTest.kt
@@ -0,0 +1,13 @@
+package com.adaptionsoft.games.trivia
+
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class AssertionsKotlinDemo {
+
+    @Test
+    fun `basic junit jupiter test`() {
+        assertTrue(false)
+    }
+
+}
