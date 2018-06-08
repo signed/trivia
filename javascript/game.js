2950dacb22c8fabe2cb93309b7893b643508923f
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2018-06-08 14:22:50 +0200

    merge java script golden master

--- javascript/game.js
+++ javascript/game.js
@@ -169,23 +169,3 @@ exports.Game = function() {
 		return true;
   };
 };
-
-var notAWinner = false;
-
-var game = new Game();
-
-game.add('Chet');
-game.add('Pat');
-game.add('Sue');
-
-do{
-
-  game.roll(Math.floor(Math.random()*6) + 1);
-
-  if(Math.floor(Math.random()*10) == 7){
-    notAWinner = game.wrongAnswer();
-  }else{
-    notAWinner = game.wasCorrectlyAnswered();
-  }
-
-}while(notAWinner);
